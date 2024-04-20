184e5aa2efb1ed373079b1ca24b55c35bff45ff4
AbishekNarayanV <abishek.v@cognizant.com>
2024-04-20 13:47:48 +0530

    First Commit of directives

---
+++ directives/product-list/product-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+ product:number[]=[0,1,2,3,4,5]
+
+}
