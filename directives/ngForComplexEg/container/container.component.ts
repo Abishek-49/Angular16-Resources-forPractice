342285aeb699993b8fadd1a0692c9f69407b22d3
AbishekNarayanV <abishek.v@cognizant.com>
2024-04-20 19:29:44 +0530

    first commit of ngComplex eg and modified the names

---
+++ directives/ngForComplexEg/container/container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.css']
+})
+export class ContainerComponent {
+  product:number[]=[0,1,2,3,4,5]
+}
